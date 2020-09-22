2839179908d40c54a5b3b852740133f16f5b7a8a
John McNay <john.w.mcnay@gmail.com>
2020-09-22 15:53:55 -0500

    basic accordion structure in place, but needs styling/formatting

--- js/movies.js
+++ js/movies.js
@@ -37,7 +37,7 @@ $("#addMovie").submit(event => {
 function loadHTML() {
     let $load = $("#load");
 
-    $("#movieTable").children().empty();
+    $("#movieTable").empty();
 
     $load.append("Loading...");
 
@@ -60,21 +60,64 @@ function generateHTML(data) {
 
     for (let obj of data) {
         let $movieCard = $(document.createElement("div"))
-            .addClass("card")
+            .addClass("card");
+
+
         let $cardHeader = $(document.createElement("div"))
             .addClass("card-header row")
+            .attr("id", `movie${obj.id}`);
+
+        // ` <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+        //     aria-expanded="true" aria-controls="collapseOne">
+        //             Collapsible Group Item #1
+        //         </button>`
+
         let $title = $(document.createElement("div")).append(obj.title)
             .addClass("col-6")
-            .click(function (){
+            .click(function () {
                 $("#titleEdit").val(obj.title);
                 $("#ratingEdit").val(obj.rating)
                 $("#submitEdit").attr("currentEdit", obj.id)
             });
 
         let $rating = $(document.createElement("div")).append(obj.rating)
-            .addClass("col-6")
-        $cardHeader.append($title, $rating)
-        $movieCard.append($cardHeader)
+            .addClass("col-6");
+
+        let $headerBtn = $(document.createElement("button"))
+            .addClass("btn btn-link collapsed")
+            .attr({
+                "type": "button",
+                "data-toggle": "collapse",
+                "data-target": `#collapse${obj.id}`,
+                "aria-expanded": "false",
+                "aria-controls": `collapse${obj.id}`,
+            }).append($title, $rating);
+
+
+        let $cardBodyDiv = $(document.createElement("div"))
+            .attr({
+                "id": `collapse${obj.id}`,
+                "data-parent": "#movieTable",
+                "aria-labelledby": `movie${obj.id}`,
+            })
+            .addClass("collapse");
+
+        let $cardBody = $(document.createElement("div"))
+            .addClass("card-body")
+            .append(obj.title);
+
+
+        $cardBodyDiv.append($cardBody);
+
+        // `<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        //     <div class="card-body">
+        //
+        //     </div>
+        // </div>`
+
+        $cardHeader.append($headerBtn);
+        $movieCard.append($cardHeader);
+        $movieCard.append($cardBodyDiv);
         let $delete = $(document.createElement("div"))
             .append("delete")
             .click(function () {
@@ -88,6 +131,7 @@ function generateHTML(data) {
     }
     // $("#movieTable").append($titles).append($ratings).append($deletions);
 }
+
 $("#editMovie").submit(event => {
     event.preventDefault();
 
@@ -123,8 +167,8 @@ function deleteItem(id) {
     }
     fetch(url, options)
         .then(response => console.log(response))
-        .then(()=>loadHTML())
-        .catch (error => console.error(error));
+        .then(() => loadHTML())
+        .catch(error => console.error(error));
 }
 
 // fetch(`https://omdbapi.com/?apikey=${omdbToken}&s=summer`)
