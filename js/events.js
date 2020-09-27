428a623bf7817f98db8b0148d07b266769709f75
Caroline E <caroline.esquivel01@gmail.com>
2020-09-27 18:49:18 -0500

    syled modals, working on accordian

--- js/events.js
+++ js/events.js
@@ -118,7 +118,7 @@ function fillData(data, div="#dataDiv") {
         let $img = $(document.createElement("img"));
         $img.attr("src", data.Poster);
         $img.attr("height", "240px");
-        $posterDiv.addClass("col-6 p-0").append($img);
+        $posterDiv.addClass("col-6 text-center").append($img);
         $topHalf.append($posterDiv);
     }
 
